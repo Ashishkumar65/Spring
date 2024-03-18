dcdf7a31fff1c263b0c3bb3c9314c41f2a3dd207
ASHISH KUMAR <83687087+Ashishkumar65@users.noreply.github.com>
2024-03-18 23:19:01 +0530

    Add files via upload

---
+++ SpringBoot/02-spring-boot-spring-coore/01-constructor-injectiom/src/main/java/com/springcoredemo/springcoredemo/Coach.java
@@ -0,0 +1,6 @@
+package com.springcoredemo.springcoredemo;
+
+public interface Coach {
+
+    String getDailyWorkout();
+}
---
+++ SpringBoot/02-spring-boot-spring-coore/01-constructor-injectiom/src/main/java/com/springcoredemo/springcoredemo/DemoController.java
@@ -0,0 +1,23 @@
+package com.springcoredemo.springcoredemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    //define a private field for dependency
+    private Coach myCoach;
+
+    //Create a constructor for injection
+    @Autowired
+    public DemoController(Coach theCoach){
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+
+}
